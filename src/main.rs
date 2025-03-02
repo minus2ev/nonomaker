4f79cd5b2f6f62049ce5bc034d49d9c973d52c08
Oleksandr Boiko <minus2ev@gmail.com>
2025-03-02 12:30:29 +0100

    Render cells

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use rand::Rng;
+
 slint::include_modules!();
 
 fn main() {
-    MainWindow::new().unwrap().run().unwrap();
+    let ui = MainWindow::new().unwrap();
+
+    ui.set_cols(30);
+    ui.set_rows(20);
+
+    let mut rng = rand::rng();
+    let cells: Vec<CellState> = (0..30 * 20)
+        .map(|_| match rng.random_range(0..3) {
+            0 => CellState::Unknown,
+            1 => CellState::Empty,
+            _ => CellState::Filled,
+        })
+        .collect();
+    let cells_model = std::rc::Rc::new(slint::VecModel::from(cells));
+    ui.set_cells(cells_model.clone().into());
+
+    ui.run().unwrap();
 }
